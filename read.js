bc26bfa8987b4385a10688c5a4fae14ccefe8792
Srijan Reddy <srijanreddy98@gmail.com>
2019-07-20 20:32:00 +0530

    Uploading to git

--- read.js
+++ read.js
@@ -1,25 +1,24 @@
 const fs = require('fs');
-let data = fs.readFileSync('a.csv').toString();
+let data = fs.readFileSync('gr').toString();
 data = data.split('\n');
-const {google} = require('../google');
-let count = 100;
-let unit = 11;
+const {google} = require('./google');
+let count = 0;
+let unit = 1;
 let net = 0;
 let main = async () => {
     let words = [];
     while(true) {
         let i = data[count];
-        console.log(count);
-        const d = i.split(',');
+        console.log(count,i);
+        const d = i;
         let goog;
         try {
-            goog = await google(d[0]);
+            goog = await google(d);
         } catch(e) {
             console.log(e)
         }
         let ex = {
-            word: d[0],
-            meaning: d[1],
+            word: d,
             google: goog,
             id: count,
             unit: unit,
@@ -27,12 +26,12 @@ let main = async () => {
             mark2: false
         }
         words.push(ex);
-        if (count % 10 === 0) unit++;
         count++;
+        if (count % 10 === 0) unit++;
         net++;
-        if (net === 1) break;
+        if (count === 3) break;
     }
-    fs.writeFileSync('first.json', JSON.stringify(words));
+    fs.writeFileSync('all.json', JSON.stringify(words));
 }
 
 main();
